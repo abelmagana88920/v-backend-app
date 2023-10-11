cadb90faed2ed04e263f1478c8824d8142f755b7
Abel Magana <abelmagana88920@gmail.com>
2023-10-11 18:58:51 +0800

    Initialized project

---
+++ README.md
@@ -0,0 +1,7 @@
+In the project directory, you can run:
+
+### `nodemon server`
+
+Runs the app in the development mode.\
+Open [http://localhost:4000](http://localhost:4000) to view it in the browser.
+ 
---
+++ server.js
@@ -0,0 +1,49 @@
+const fileupload = require("express-fileupload");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    fileupload({
+        createParentPath: true,
+    }),
+);
+
+app.post("/upload", uploadFiles);
+
+async function uploadFiles(req, res) {
+
+    const {body} = req;
+    console.log(req.body);
+    console.log(req.files)
+  
+    try {
+        if (!req.files) {
+            res.send({
+                status: "failed",
+                message: "No file uploaded",
+            });
+        } else {
+            let file = req.files.file;
+            const buffer = file.data;
+
+            res.send(`<pre>
+Make: ${body.make}
+Model: ${body.model}
+Badge: ${body.badge}
+
+Logbook:
+
+${buffer.toString('utf8')}
+                </pre>`);
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
+
+app.listen(4000, () => {
+    console.log(`Server started...`);
+});
